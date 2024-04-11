3172e2810ab993a9c4f6da2351463b4dcdab6335
GH-2018-prod <maancuza@gmail.com>
2024-04-10 18:02:53 -0600

    updating nav and meta data

---
+++ src/NavigationBar.jsx
@@ -0,0 +1,14 @@
+export default function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">MENU</a></li>
+        <li><a href="">RESERVATION</a></li>
+        <li><a href="">ORDER ONLINE</a></li>
+        <li><a href="">LOG IN</a></li>
+      </ul>
+    </nav>
+  )
+}
